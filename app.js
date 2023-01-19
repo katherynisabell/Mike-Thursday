fea4ddb613cfc0e4092dfa605e9886f8accd83fd
Grant <kgrant@una.edu>
2023-01-19 16:12:54 -0600

    put commit msg here

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World This is Barry 2!')
+})
+
+console.log('in the node console');
+
+app.listen(3000, () => {
+    console.log(`Server is running & listening on port: 3000`);
+  });
