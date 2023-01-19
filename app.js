a09643197ebcd23b1a5350f7882de479a948c6f3
Grant <kgrant@una.edu>
2023-01-19 16:44:41 -0600

    commiting new port listener

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-
+const PORT = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
-  res.send('Hello World This is Barry 2!')
+  res.send('Katheryn White ! :)')
 })
 
 console.log('in the node console');
 
-app.listen(3000, () => {
-    console.log(`Server is running & listening on port: 3000`);
+app.listen(PORT, () => {
+    console.log(`Server is running & listening on port: ${PORT}`);
   });
